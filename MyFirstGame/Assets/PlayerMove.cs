c69d3eaf2d0e214c03e0ead3ff30785b9ed67d43
18jminkim <zzzjmk@kaist.ac.kr>
2020-07-31 22:08:31 +0900

    working on roll

--- MyFirstGame/Assets/PlayerMove.cs
+++ MyFirstGame/Assets/PlayerMove.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Data.Odbc;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
@@ -9,13 +10,38 @@ public class PlayerMove : MonoBehaviour
 
 
     // movement
-    public float runSpeed = 1f;
     public bool movable = true;
+    public float runSpeed = 1f;
+
+
+    // sprinting
+    public float sprintSpeed = 3f;
+    public bool sprinting = false;
+
+
+    public float buttonCoolerW = 0.5f;
+    private int buttonCountW = 0;
+    public float buttonCoolerA = 0.5f;
+    private int buttonCountA = 0;
+    public float buttonCoolerS = 0.5f;
+    private int buttonCountS = 0;
+    public float buttonCoolerD = 0.5f;
+    private int buttonCountD = 0;
+
+
+
+    public string lastDown = " ";
+    public bool doubleTap = false;
+
 
 
     // rotation
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
+
+    // roll
+    public bool roll;
+
 
     // animations
     public Animator animator;
@@ -34,13 +60,20 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        doubleW();
+        doubleA();
+        doubleS();
+        doubleD();
+
+
+
     }
 
 
     private void FixedUpdate()
     {
         move();
+
     }
 
 
@@ -52,7 +85,8 @@ public class PlayerMove : MonoBehaviour
         float x = Input.GetAxisRaw("Horizontal");
         float z = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(x, 0f, z).normalized;
-        transform.Translate(direction * runSpeed * Time.deltaTime, Space.World);
+
+
 
 
         //rotation
@@ -64,14 +98,174 @@ public class PlayerMove : MonoBehaviour
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
             //run = true;
             animator.SetBool("run", true);
+
+
+            if (sprinting)
+            {
+                animator.SetBool("sprint", true);
+            }
+
+
+
+
+        }
+        else // character not moving
+        {
+            sprinting = false;
+            animator.SetBool("sprint", false);
+            animator.SetBool("run", false);
+            //Debug.Log("Not moving.");
+
+
+        }
+
+        // roll.
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("roll");
+        }
+
+
+        if (sprinting)
+        {
+            transform.Translate(direction * sprintSpeed * Time.deltaTime, Space.World);
+
         }
         else
         {
-            //run = false;
-            animator.SetBool("run", false);
+            transform.Translate(direction * runSpeed * Time.deltaTime, Space.World);
+
         }
 
 
-
     }
+
+
+    private void doubleW()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            buttonCountA = 0;
+            buttonCountS = 0;
+            buttonCountD = 0;
+            if (buttonCoolerW > 0 && buttonCountW == 1/*Number of Taps you want Minus One*/)
+            {
+                //Has double tapped
+
+                sprinting = true;
+
+            }
+            else
+            {
+                buttonCoolerW = 0.5f;
+                buttonCountW += 1;
+            }
+        }
+        if (buttonCoolerW > 0)
+        {
+            buttonCoolerW -= 1 * Time.deltaTime;
+        }
+        else
+        {
+            buttonCountW = 0;
+        }
+    }
+
+    private void doubleA()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            buttonCountW = 0;
+            buttonCountS = 0;
+            buttonCountD = 0;
+
+
+            if (buttonCoolerA > 0 && buttonCountA == 1/*Number of Taps you want Minus One*/)
+            {
+                //Has double tapped
+
+                sprinting = true;
+
+            }
+            else
+            {
+                buttonCoolerA = 0.5f;
+                buttonCountA += 1;
+            }
+        }
+        if (buttonCoolerA > 0)
+        {
+            buttonCoolerA -= 1 * Time.deltaTime;
+        }
+        else
+        {
+            buttonCountA = 0;
+        }
+    }
+
+    private void doubleS()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            buttonCountW = 0;
+            buttonCountA = 0;
+            buttonCountD = 0;
+            if (buttonCoolerS > 0 && buttonCountS == 1/*Number of Taps you want Minus One*/)
+            {
+                //Has double tapped
+
+                sprinting = true;
+
+            }
+            else
+            {
+                buttonCoolerS = 0.5f;
+                buttonCountS += 1;
+            }
+        }
+        if (buttonCoolerS > 0)
+        {
+            buttonCoolerS -= 1 * Time.deltaTime;
+        }
+        else
+        {
+            buttonCountS = 0;
+        }
+    }
+
+    private void doubleD()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            buttonCountW = 0;
+            buttonCountA = 0;
+            buttonCountS = 0;
+            if (buttonCoolerD > 0 && buttonCountD == 1/*Number of Taps you want Minus One*/)
+            {
+                //Has double tapped
+
+                sprinting = true;
+
+            }
+            else
+            {
+                buttonCoolerD = 0.5f;
+                buttonCountD += 1;
+            }
+        }
+        if (buttonCoolerD > 0)
+        {
+            buttonCoolerD -= 1 * Time.deltaTime;
+        }
+        else
+        {
+            buttonCountD = 0;
+        }
+    }
+
+   
+
+
+
+
 }
