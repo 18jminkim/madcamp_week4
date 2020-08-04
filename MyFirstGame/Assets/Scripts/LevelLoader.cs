227a385d1fec1d4705b3364e4e80b5515761d4a0
hana-0510 <hana000510@naver.com>
2020-08-05 01:03:23 +0900

    scene change

---
+++ MyFirstGame/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    public Animator transition;
+    public float transitionTime = 1f;
+    public Collider doorCol;
+
+
+    private void Start()
+    {
+        doorCol = GetComponent<Collider>();
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("collided");
+            LoadNextLevel();
+        }
+        
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine ( LoadLevel(SceneManager.GetActiveScene().buildIndex + 1) );
+    }
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        //Play Ani
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+
+    }
+}
--- MyFirstGame/Assets/Scripts/PauseGame.cs
+++ MyFirstGame/Assets/Scripts/PauseGame.cs
@@ -9,6 +9,11 @@ public class PauseGame : MonoBehaviour
 
     public GameObject pauseMenuUI;
 
+    void Start()
+    {
+        Resume();
+    }
+
     // Update is called once per frame
     void Update()
     {
